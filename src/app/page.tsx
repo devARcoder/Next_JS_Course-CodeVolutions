916d0c60bb17c74326dff0b5d058add1717ed6de
Abdur Razzaq <devarcoder789@gmail.com>
2026-02-10 23:03:59 +0500

    lecture 19 completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Welcome to My Next.js App!</h1>
       <p className="mt-4 text-lg text-gray-600">This is the home page.</p>
+
+      <Link href="/blog">Blog</Link>
     </div>
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,15 @@
+import Link from "next/link"
 export default function Products() {
+    const productId = 100;
     return (
         <>
-
+        <Link href="/">Back to Home</Link>
         <h1>Products List</h1>
         <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
+            <li><Link href="products/1">Product 1</Link></li>
+            <li><Link href="products/2">Product 2</Link></li>
+            <li><Link href="products/3" replace>Product 3</Link></li>
+            <li><Link href={`products/${productId}`}>Product {productId}</Link></li>
         </ul>
         </>
         
