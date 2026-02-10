1cc3db3404bad1cd19c1959e1fddaf79eb388539
Abdur Razzaq <devarcoder789@gmail.com>
2026-02-11 00:51:12 +0500

    lecture 25 completed

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,6 +1,17 @@
 "use client";
-export default function ErrorBoundary(){
-    return (
-        <h1>Error loading in review</h1>
-    )
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <div>
+      <h1>Something went wrong</h1>
+      <p>{error.message}</p>
+      <button onClick={reset}>Try Again</button>
+    </div>
+  );
 }
